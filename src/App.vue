24a9e0a92f025e34d30edbc2f8bb5be54b256e09
FoskyM <i@fosky.top>
2025-01-03 15:34:10 +0800

    feat: back button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-
+import router from '@/router'
 import CheckBrowser from '@/components/CheckBrowser'
 </script>
 
 <template>
+  <div class="absolute top-4 left-4" v-if="router.currentRoute.value.path != '/'">
+    <button class="btn btn-primary" @click="router.push('/')">返回</button>
+  </div>
   <div class="container mx-auto px-4 py-12 h-full">
     <CheckBrowser />
     <RouterView />
