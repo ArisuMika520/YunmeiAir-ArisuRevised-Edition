7e143023d820d6f589ef60d953785255188fec9e
FoskyM <i@fosky.top>
2025-01-03 18:57:46 +0800

    fix: unlock

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { RouterLink } from 'vue-router'
 import { pausableWatch, useBluetooth } from '@vueuse/core'
 
@@ -11,9 +11,10 @@ const userDataStore = useUserDataStore()
 
 const memo = ref<string>('等待开锁')
 const progress = ref<number>(0)
+const errorMsg = ref<string>('')
 
-const currentLockServ = ref('none')
-const isUnlocking = ref(false)
+const currentLockServ = ref<string>('none')
+const isUnlocking = ref<boolean>(false)
 
 if (userDataStore.lockList.length === 0) {
   memo.value = '请先添加门锁'
@@ -22,9 +23,8 @@ if (userDataStore.lockList.length === 0) {
   memo.value = '等待开锁'
 }
 
-const { isConnected, requestDevice, server } = useBluetooth({
-  acceptAllDevices: true,
-  optionalServices: [currentLockServ],
+const { isConnected, requestDevice, server, error } = useBluetooth({
+  filters: [{ services: [currentLockServ.value.toLowerCase()] }],
 })
 
 const update = (_progress: number, _memo: string) => {
@@ -33,35 +33,54 @@ const update = (_progress: number, _memo: string) => {
 }
 
 async function unlock() {
-  isUnlocking.value = true
   try {
     const currentLock = userDataStore.lockList.find(
       (lock: Lock) => lock.D_SERV == currentLockServ.value,
     )
-    // @ts-expect-error getPrimaryService
-    const service = await server.getPrimaryService(currentLockServ)
+    const service = await server.value.getPrimaryService(currentLockServ.value.toLowerCase())
     update(50, '查询中')
-    const characteristic = await service.getCharacteristic(currentLock?.D_CHAR)
+    const characteristic = await service.getCharacteristic(currentLock?.D_CHAR.toLowerCase())
+    console.log(characteristic)
     update(60, '查询成功，解锁中')
     const pwd = getPwd(currentLock?.D_SEC || '')
-    await characteristic.sendValue(pwd)
+    await characteristic.writeValue(pwd)
     update(100, '完成')
   } catch (error) {
     console.error(error)
+    errorMsg.value = error as string
     update(0, '出错')
   }
   isUnlocking.value = false
 }
 
 const connect = () => {
+  isUnlocking.value = true
   update(10, '连接中')
   requestDevice()
 }
 
-const { stop } = pausableWatch(isConnected, (newIsConnected) => {
-  if (!newIsConnected || !server.value || isUnlocking.value) return
+watch(error, (newVal) => {
+  console.log(newVal)
+  errorMsg.value = newVal as string
+  if (newVal !== null) {
+    if (newVal == 'NotFoundError: Bluetooth adapter not available.') {
+      memo.value = '未找到蓝牙适配器'
+    } else if (newVal == 'NotFoundError: User cancelled the requestDevice() chooser.') {
+      memo.value = '您取消了选择'
+    } else if (newVal == 'NetworkError: Connection Error: Connection attempt failed.') {
+      memo.value = '连接失败'
+    }
+
+    isUnlocking.value = false
+    progress.value = 0
+  }
+})
+
+const { stop } = pausableWatch(isConnected, async (newIsConnected) => {
+  console.log(newIsConnected)
+  if (!newIsConnected || !server.value || !isUnlocking.value) return
   update(30, '找到设备')
-  unlock()
+  await unlock()
   stop()
 })
 </script>
@@ -92,11 +111,16 @@ const { stop } = pausableWatch(isConnected, (newIsConnected) => {
         <button
           class="btn btn-primary w-full my-2"
           @click="connect()"
-          :disabled="currentLockServ == 'none'"
+          :disabled="currentLockServ == 'none' || isUnlocking"
         >
-          开门
+          <template v-if="isUnlocking">开门中...</template>
+          <template v-else>开门</template>
         </button>
         <label for="modal-login" class="btn btn-ghost w-full">添加门锁</label>
+      </div>
+
+      <div class="error-msg bg-[#eecc00] text-white p-4 w-full rounded-lg" v-if="errorMsg !== ''">
+        {{ errorMsg }}
       </div>
     </div>
   </div>
